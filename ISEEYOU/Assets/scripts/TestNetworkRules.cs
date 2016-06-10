b7a6e4b95954ec3b32d7d41fbefea8af32573e90
jbiskur <jbiskur@gmail.com>
2016-06-10 22:48:14 +0100

    Network Basics

---
+++ ISEEYOU/Assets/scripts/TestNetworkRules.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class TestNetworkRules : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    if (isLocalPlayer)
+        {
+            GetComponent<TestNetworkMovement>().enabled = true;
+        }
+	}
+}
