5c786e5bd707b1ef84dc1280271fd770d6e2aa5b
Ian T. Jacobsen <ian@itjac.me>
2016-06-10 23:40:20 +0100

    Trap code. Waiting dependencies

---
+++ ISEEYOU/Assets/scripts/StunTrap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StunTrap : Trap {
+	public GameObject gas_prefab;
+
+	protected override void Explode(GameObject player) {
+		GameObject inst;
+
+		if (gas_prefab) {
+			inst = Instantiate(gas_prefab);
+			inst.transform.position = GetComponent<Transform>().position;
+			inst.GetComponent<ParticleSystem>().Play();
+		}
+
+		// TODO call into burglar
+	}
+}
