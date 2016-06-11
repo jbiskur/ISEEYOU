60e35c3845a92f83e7c07a665f9df1a81185002f
jbiskur <jbiskur@gmail.com>
2016-06-11 12:08:47 +0100

    Added IP Canvas

---
+++ ISEEYOU/Assets/scripts/IpText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class IpText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Text>().text = Network.player.ipAddress;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
